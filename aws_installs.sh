96dbf1a4cf75baba704dee232302389efe4675c0
Nathan Mori <nathanmori@gmail.com>
2016-07-26 16:41:03 -0700

    update aws_installs

--- aws_installs.sh
+++ aws_installs.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
+
 sudo apt-get install awscli
 sudo apt-get install ipython
 sudo apt-get install git
+sudo apt-get install yum
+sudo apt-get install gcc
+sudo apt-get install python-setuptools
+sudo apt-get install python-pip
+sudo apt-get install postgres-xc
+sudo apt-get install libpq-dev python-dev
+sudo pip install psycopg2
+sudo pip install pandas
+
+
 git clone https://github.com/nathanmori/Linking-Profiles-Across-Social-Media-Sites-By-User.git
