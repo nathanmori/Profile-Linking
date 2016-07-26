968e0190ee24d10cf7863a48340db03b4085940c
Nathan Mori <nathanmori@gmail.com>
2016-07-26 15:23:38 -0700

    create aws_installs script

---
+++ aws_installs.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+sudo apt-get install awscli
+sudo apt-get install ipython
+sudo apt-get install git
+git clone https://github.com/nathanmori/Linking-Profiles-Across-Social-Media-Sites-By-User.git
